33cc803fa58d3b5c400808d23d7efaacfe9fd503
jake1970 <jakeyjyoung@gmail.com>
2023-08-20 16:47:10 +0200

    Initial Setup

--- app/src/main/java/com/cjras/thepresentmovement/MainActivity.kt
+++ app/src/main/java/com/cjras/thepresentmovement/MainActivity.kt
@@ -2,10 +2,28 @@ package com.cjras.thepresentmovement
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.cjras.thepresentmovement.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+//-----------------------------------------------------------------------//
+
+        //Set view binding
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //Hide the action bar
+        supportActionBar?.hide()
+
+//-----------------------------------------------------------------------//
+
     }
 }
+
+//-----------------------------------------------------------------------//
+//Notes:
+//-----------------------------------------------------------------------//
+//prep page method to hide the top bar and change default color?
+//probably in its own class
+//-----------------------------------------------------------------------//
