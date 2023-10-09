5b5739972f5996e8d87120c559d004089195a306
jake1970 <jakeyjyoung@gmail.com>
2023-10-09 19:41:53 +0200

    Started Notices Screen

--- app/src/main/java/com/cjras/thepresentmovement/notices.kt
+++ app/src/main/java/com/cjras/thepresentmovement/notices.kt
@@ -1,15 +1,17 @@
 package com.cjras.thepresentmovement
 
+
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Space
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import com.cjras.thepresentmovement.databinding.FragmentNoticesBinding
+import com.google.android.material.datepicker.MaterialDatePicker
+import java.text.SimpleDateFormat
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
@@ -17,43 +19,94 @@ private const val ARG_PARAM2 = "param2"
  * create an instance of this fragment.
  */
 class notices : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _binding: FragmentNoticesBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_notices, container, false)
+
+        _binding = FragmentNoticesBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+
+        binding.tvStartDate.setOnClickListener(){
+            datePicker(true, binding.tvStartDate)
+        }
+
+        binding.tvEndDate.setOnClickListener(){
+            datePicker(false, binding.tvEndDate)
+        }
+
+        //---------------------------------------------------------------------------------------------
+        //test code for custom components
+        //---------------------------------------------------------------------------------------------
+        for (i in 1..8) {
+
+            val activityLayout = binding.llNotices;
+            var newAnnouncement = announcement_card(activity)
+
+            newAnnouncement.binding.tvAnnouncementTime.text = "10/12/23"
+            newAnnouncement.binding.tvAnnouncementTitle.text = "Changes to the Think For Good Event"
+            newAnnouncement.binding.tvAnnouncementText.text = "Hello Members\n" +
+                    "The think for good event has been postponed until the 26th, I have updated the card in the present movements app.\n" +
+                    "Take care\n" +
+                    "{Person}"
+
+            //add the new view
+            activityLayout.addView(newAnnouncement)
+
+            val scale = requireActivity().resources.displayMetrics.density
+            val pixels = (14 * scale + 0.5f)
+
+            val spacer = Space(activity)
+            spacer.minimumHeight = pixels.toInt()
+            activityLayout.addView(spacer)
+
+        }
+        //---------------------------------------------------------------------------------------------
+
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment notices.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            notices().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    private fun datePicker(startPrompt: Boolean, entryField: TextView)
+    {
+
+        var entryPrompt = "Select an end date"
+
+        if (startPrompt)
+        {
+            val entryPrompt = "Select a start date"
+        }
+
+        val builder = MaterialDatePicker.Builder.datePicker()
+        builder.setTitleText(entryPrompt)
+        val picker = builder.build()
+        picker.show(childFragmentManager, picker.toString())
+
+        picker.addOnPositiveButtonClickListener {
+            // Respond to positive button click.
+            val selectedDate = SimpleDateFormat("dd/MM/yy")
+            entryField.text = selectedDate.format(picker.selection)
+        }
+
+        picker.addOnNegativeButtonClickListener {
+            // Respond to negative button click.
+            entryField.text = getString(R.string.blankDate)
+        }
+
+        picker.addOnCancelListener {
+            // Respond to cancel button click.
+            entryField.text = getString(R.string.blankDate)
+        }
     }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+
+
 }
