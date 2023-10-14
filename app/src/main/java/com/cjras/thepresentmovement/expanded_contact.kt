29a30ce6cb123ee6ed2b7426f43b68accdf38e25
jake1970 <jakeyjyoung@gmail.com>
2023-10-14 14:37:20 +0200

    Added Camera and Photo Library options for setting contact profile image

--- app/src/main/java/com/cjras/thepresentmovement/expanded_contact.kt
+++ app/src/main/java/com/cjras/thepresentmovement/expanded_contact.kt
@@ -1,12 +1,28 @@
 package com.cjras.thepresentmovement
 
+import android.Manifest
+import android.app.Activity
+import android.app.AlertDialog
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.net.Uri
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import android.os.Environment
+import android.provider.MediaStore
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.cjras.thepresentmovement.databinding.FragmentContactsBinding
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.Fragment
 import com.cjras.thepresentmovement.databinding.FragmentExpandedContactBinding
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 /**
@@ -19,6 +35,13 @@ class expanded_contact : Fragment() { //R.layout.fragment_expanded_contact
     private var _binding: FragmentExpandedContactBinding? = null
     private val binding get() = _binding!!
 
+    //----------------------------------------------------------------------------------------------------
+    companion object {
+        private const val CAMERA_PERMISSION_CODE = 100
+        private const val CAMERA_REQUEST_CODE = 200
+        private const val PICK_FROM_GALLERY = 1
+    }
+    //----------------------------------------------------------------------------------------------------
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -27,6 +50,28 @@ class expanded_contact : Fragment() { //R.layout.fragment_expanded_contact
 
         _binding = FragmentExpandedContactBinding.inflate(inflater, container, false)
         val view = binding.root
+
+
+        //----------------------------------------------------------------------------------------------------
+
+
+
+        binding.ivMyProfileImageTint.setOnClickListener()
+        {
+            //method to add an image
+            handlePhoto()
+        }
+
+        binding.tvMyProfileImageEditText.setOnClickListener()
+        {
+            //method to add an image
+            handlePhoto()
+        }
+
+
+
+        //----------------------------------------------------------------------------------------------------
+
 
 
         //----------------------------------------------------------------------------------------------------
@@ -64,4 +109,153 @@ class expanded_contact : Fragment() { //R.layout.fragment_expanded_contact
         _binding = null
     }
 
+    //---------------------------------------------------------------------------------------------------------------------------------------------------------
+    //Camera Functions
+    //---------------------------------------------------------------------------------------------------------------------------------------------------------
+
+    //---------------------------------------------------------------------------------------------------------------------------------------------------------
+    //Method to handle selecting and starting an image source for the contact photo
+    //---------------------------------------------------------------------------------------------------------------------------------------------------------
+    private fun handlePhoto() {
+
+        //new dialog
+        val builder = AlertDialog.Builder(activity)
+
+        //set the dialog title
+        builder.setTitle(R.string.imageSourcePrompt)
+
+        //set the source options for the dialog
+        builder.setItems(R.array.imageSources) { dialog, which ->
+
+            if (which == 0)
+            {
+                //if 0 then the user wants to take a new photo
+                //call camera
+                //check permissions
+                if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED) {
+
+                    //call open camera method
+                    startCamera()
+                } else {
+                    ActivityCompat.requestPermissions(requireActivity(), arrayOf(Manifest.permission.CAMERA), CAMERA_PERMISSION_CODE)
+                }
+
+            }
+            else
+            {
+                if (which == 1)
+                {
+
+                    //if 1 then the user wants to select an existing photo
+                    //call photo library
+                    //check permissions
+                    if (ContextCompat.checkSelfPermission(requireContext(), Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED) {
+
+                        //call image selector method
+                        startImageSelector()
+                    } else {
+                        ActivityCompat.requestPermissions(
+                            requireActivity(), arrayOf<String>(Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE), PICK_FROM_GALLERY
+                        )
+                    }
+
+
+                }
+            }
+
+        }
+
+        //show the dialog
+        builder.show()
+    }
+
+
+    //---------------------------------------------------------------------------------------------------------------------------------------------------------
+    //Method to open image picker
+    //---------------------------------------------------------------------------------------------------------------------------------------------------------
+    private fun startImageSelector() {
+        //val gallery = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI)
+        val galleryIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.INTERNAL_CONTENT_URI) //EXTERNAL_CONTENT_URI
+        //@Suppress("DEPRECATION")
+        //startActivityForResult(gallery, PICK_FROM_GALLERY)
+
+        if (galleryIntent.resolveActivity(requireActivity().packageManager) != null) {
+            @Suppress("DEPRECATION")
+            startActivityForResult(galleryIntent, PICK_FROM_GALLERY);
+        } else {
+            Toast.makeText(requireContext(), "Photo Library is not available", Toast.LENGTH_SHORT).show()
+        }
+
+    }
+    //---------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+    //---------------------------------------------------------------------------------------------------------------------------------------------------------
+    //Method to start camera
+    //---------------------------------------------------------------------------------------------------------------------------------------------------------
+    private fun startCamera() {
+        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        if (cameraIntent.resolveActivity(requireActivity().packageManager) != null) {
+            @Suppress("DEPRECATION")
+            startActivityForResult(cameraIntent, CAMERA_REQUEST_CODE)
+        } else {
+            Toast.makeText(requireContext(), "Camera is not available", Toast.LENGTH_SHORT).show()
+        }
+    }
+    //---------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+    //---------------------------------------------------------------------------------------------------------------------------------------------------------
+    //Catch Finished Activity
+    //---------------------------------------------------------------------------------------------------------------------------------------------------------
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        @Suppress("DEPRECATION")
+        super.onActivityResult(requestCode, resultCode, data)
+        if ((requestCode == CAMERA_REQUEST_CODE && resultCode == Activity.RESULT_OK) || (requestCode == PICK_FROM_GALLERY && resultCode == Activity.RESULT_OK)) {
+
+            var imageBitmap = data?.extras?.get("data") as Bitmap?
+
+            if (imageBitmap == null)
+            {
+                val imageUri = data?.data
+                imageBitmap = MediaStore.Images.Media.getBitmap(activity?.contentResolver, Uri.parse(imageUri.toString()))
+            }
+
+            binding.ivMyProfileImage.setImageBitmap(imageBitmap)
+            saveImageLocally(imageBitmap)
+
+
+        }
+
+    }
+    //---------------------------------------------------------------------------------------------------------------------------------------------------------
+
+    //---------------------------------------------------------------------------------------------------------------------------------------------------------
+    //Method Save Captured Image On Device Storage
+    //---------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+    private fun saveImageLocally(imageBitmap: Bitmap?) {
+        val timeStamp = SimpleDateFormat("yyyyMMdd_HHmmss", Locale.getDefault()).format(Date())
+        val imageFileName = "IMG_$timeStamp.jpg"
+
+        val storageDir = requireContext().getExternalFilesDir(Environment.DIRECTORY_PICTURES)
+        val imageFile = File(storageDir, imageFileName)
+
+        //return view
+        try {
+            val fileOutputStream = FileOutputStream(imageFile)
+            imageBitmap?.compress(Bitmap.CompressFormat.JPEG, 100, fileOutputStream)
+            fileOutputStream.close()
+
+            Toast.makeText(requireContext(), "Image saved successfully", Toast.LENGTH_SHORT).show()
+        } catch (e: IOException) {
+            e.printStackTrace()
+            Toast.makeText(requireContext(), "Failed to save image", Toast.LENGTH_SHORT).show()
+        }
+    }
+    //---------------------------------------------------------------------------------------------------------------------------------------------------------
+
+    //---------------------------------------------------------------------------------------------------------------------------------------------------------
 }
