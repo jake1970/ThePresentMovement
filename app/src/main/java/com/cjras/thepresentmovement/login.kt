21bc1b27a39de8bdf2e1f0f4ee91dd18e71ed6cc
RobboVC <robynma03@gmail.com>
2023-10-15 14:51:08 +0200

    Pushing login.kt

---
+++ app/src/main/java/com/cjras/thepresentmovement/login.kt
@@ -0,0 +1,18 @@
+package com.cjras.thepresentmovement
+
+import android.R
+import android.graphics.Color
+import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.ForegroundColorSpan
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+
+class login : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
