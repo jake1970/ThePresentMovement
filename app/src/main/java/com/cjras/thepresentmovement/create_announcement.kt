caf3a49a29f977dff526d86ebe441ca15d630351
jake1970 <jakeyjyoung@gmail.com>
2023-10-30 14:28:01 +0200

    Fixed Create Announcement View Binding

--- app/src/main/java/com/cjras/thepresentmovement/create_announcement.kt
+++ app/src/main/java/com/cjras/thepresentmovement/create_announcement.kt
@@ -4,9 +4,25 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.RelativeLayout
+import android.widget.Toast
+import androidx.core.view.children
 import androidx.fragment.app.Fragment
+import com.cjras.thepresentmovement.databinding.FragmentAdminBinding
+import com.cjras.thepresentmovement.databinding.FragmentCreateAnnouncementBinding
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.MainScope
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import java.time.LocalDate
 
 class create_announcement : Fragment() {
+
+    private var _binding: FragmentCreateAnnouncementBinding? = null
+    private val binding get() = _binding!!
+
+
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -14,8 +30,12 @@ class create_announcement : Fragment() {
         savedInstanceState: Bundle?,
     ): View? {
 
-        val root = inflater.inflate(R.layout.fragment_create_announcement, container, false);
+        _binding = FragmentCreateAnnouncementBinding.inflate(inflater, container, false)
+        val view = binding.root
 
-        return root;
+
+
+
+        return view;
     }
 }
