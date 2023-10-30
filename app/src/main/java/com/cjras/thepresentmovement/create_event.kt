e5b524ff1c5f944f536d8da872eb097dddb0088f
RobboVC <robynma03@gmail.com>
2023-10-31 00:02:04 +0200

    General fixing of fragment UI, added fragment_create_event.xml and almost completed fragment_add_project.xml (tbc)

---
+++ app/src/main/java/com/cjras/thepresentmovement/create_event.kt
@@ -0,0 +1,21 @@
+package com.cjras.thepresentmovement
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.cjras.thepresentmovement.databinding.FragmentSettingsBinding
+
+
+class create_event: Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val root = inflater.inflate(R.layout.fragment_create_event, container, false)
+        return root
+    }
+}
